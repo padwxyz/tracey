4cc7e0e51b58ebb683e1654c15dd0c84b5102e4b
Ananda Darma <anandadarma2612@gmail.com>
2025-01-21 22:42:44 +0800

    feat: create migrations for all models and adjusting the data types accordingly

---
+++ database/migrations/2025_01_21_130542_create_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->string('item_name');
+            $table->integer('quantity');
+            $table->enum('condition', ['Normal', 'Not normal']);
+            $table->string('brand');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
---
+++ database/migrations/2025_01_21_130733_create_notes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('admin_id')->constrained()->onDelete('cascade');
+            $table->foreignId('location_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('item_id')->constrained();
+            $table->string('title');
+            $table->date('date');
+            $table->time('time');
+            $table->string('problem');
+            $table->string('activity');
+            $table->enum('status', ['todo', 'pending', 'inprogress', 'done', 'cancel']);
+            $table->text('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
