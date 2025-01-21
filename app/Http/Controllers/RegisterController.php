1a5e429cacf70238c59b3c0b325e85bfa5a044e9
Ananda Darma <anandadarma2612@gmail.com>
2025-01-21 23:32:38 +0800

    feat: auth controller

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('', [
+            'title' => 'Register'
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required| min:3| max:255',
+            'email' => 'required| email:dns| unique:users',
+            'password' => 'required| min:5| max:255',
+            'password_confirmation' => 'required| same:password',
+            'gender' => 'required| in:male,female',
+            'phone_number' => 'required| regex:/^([0-9\s\-\+\(\)]*)$/| min:10 '
+        ]);
+
+        $validated['password'] = Hash::make($validated['password']);
+        User::create($validated);
+
+        return redirect('/')->with('success', 'Registered Successfully, Please Login');
+    }
+}
