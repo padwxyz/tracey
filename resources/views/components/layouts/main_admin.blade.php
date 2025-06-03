1cc022b14bd282d2aa3caf73980c90dc3e7d0cd2
Ananda Darma <anandadarma2612@gmail.com>
2025-06-03 19:00:41 +0800

    feat: adding apline js and body structure

--- resources/views/components/layouts/main_admin.blade.php
+++ resources/views/components/layouts/main_admin.blade.php
@@ -17,13 +17,16 @@
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body style="font-family: 'Plus Jakarta Sans', sans-serif;">
     @include('components.partials.sidebar_admin')
-    @yield('container')
-</body>
+    <main class="w-full">
+        @yield('container')
+    </main>
 
 </html>
