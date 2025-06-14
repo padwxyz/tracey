94f315fd9619bba3feb5fc9a9954d38c9c550c43
Ananda Darma <anandadarma2612@gmail.com>
2025-06-14 21:08:10 +0800

    fix: log pagination, log item select and log search

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -7,9 +7,26 @@ use App\Models\Item;
 
 class LogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $items = Item::with('category.facility.location')->paginate(10);
+        $entries = $request->input('entries', 10);
+        $search = $request->input('search');
+
+        $query = Item::with('category.location');
+
+        if ($search) {
+            $query->where(function ($q) use ($search) {
+                $q->where('item_name', 'like', "%{$search}%")
+                    ->orWhereHas('category', function ($q2) use ($search) {
+                        $q2->where('category_name', 'like', "%{$search}%")
+                            ->orWhereHas('location', function ($q3) use ($search) {
+                                $q3->where('location_name', 'like', "%{$search}%");
+                            });
+                    });
+            });
+        }
+
+        $items = $query->paginate($entries)->appends($request->all());
         $title = 'Log Status';
 
         return view('pages.user.log', compact('items', 'title'));
